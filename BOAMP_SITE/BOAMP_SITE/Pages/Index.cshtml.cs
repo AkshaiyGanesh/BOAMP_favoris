42ba71780521f518efdca3e7b6c0cdd446bfe65e
Akshaiy Ganesh <akshaiyganesh@MacBook-Air-de-Akshaiy.local>
2024-03-04 17:06:58 +0000

    Nouvelle Version du site

---
+++ BOAMP_SITE/BOAMP_SITE/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BOAMP_SITE.Pages;
+
+public class IndexModel2 : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel2(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
+
